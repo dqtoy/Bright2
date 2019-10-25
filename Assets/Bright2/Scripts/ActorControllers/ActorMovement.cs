2b6a134e6fed3d0228d6f3f94efdc829464d6b14
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-26 00:41:19 +0900

    重力処理とステージに衝突したら重力リセットされるまで

--- Assets/Bright2/Scripts/ActorControllers/ActorMovement.cs
+++ Assets/Bright2/Scripts/ActorControllers/ActorMovement.cs
@@ -11,9 +11,17 @@ namespace HK.Bright2.ActorControllers
         [SerializeField]
         private BoxCollider2D boxCollider2D = default;
 
+        [SerializeField]
+        private LayerMask raycastIncludeLayerMask = default;
+
+        [SerializeField]
+        private float gravity = default;
+
         private Actor actor;
 
         private Vector2 velocity;
+
+        private float currentGravity = 0.0f;
 
         void Awake()
         {
@@ -25,14 +33,67 @@ namespace HK.Bright2.ActorControllers
 
         void Update()
         {
+            this.AddGravity();
+
             var t = this.actor.CachedTransform;
+            this.CheckGround();
             t.localPosition += new Vector3(this.velocity.x, this.velocity.y, 0.0f);
+
+#if UNITY_EDITOR
+            this.lastVelocity = this.velocity;
+#endif
+
             this.velocity = Vector2.zero;
         }
+
+#if UNITY_EDITOR
+
+        private Vector2 lastVelocity;
+
+        void OnDrawGizmosSelected()
+        {
+            if(this.actor == null)
+            {
+                return;
+            }
+
+            var from = this.actor.CachedTransform.position;
+            var to = from + new Vector3(this.lastVelocity.x, this.lastVelocity.y, 0.0f);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(from, to);
+        }
+#endif
 
         public void AddMove(Vector2 velocity)
         {
             this.velocity += velocity;
+        }
+
+        private void AddGravity()
+        {
+            this.currentGravity += this.gravity * Time.deltaTime;
+            this.velocity += Vector2.down * this.currentGravity * Time.deltaTime;
+        }
+
+        private void CheckGround()
+        {
+            var t = this.actor.CachedTransform;
+            var pos = t.localPosition;
+            var origin = new Vector2(pos.x, pos.y) + this.boxCollider2D.offset;
+            var size = this.boxCollider2D.size;
+            var angle = t.localRotation.eulerAngles.z;
+            var direction = Vector2.down;
+            var distance = Mathf.Abs(this.velocity.y);
+            var raycastHit2D = Physics2D.BoxCast(origin, size, angle, direction, distance, raycastIncludeLayerMask.value);
+            if(raycastHit2D.transform != null)
+            {
+                var v = this.velocity;
+                v.y = 0.0f;
+                this.velocity = v;
+                this.currentGravity = 0.0f;
+                Debug.Log($"Hit {raycastHit2D.transform.name}");
+                t.localPosition = new Vector3(pos.x, raycastHit2D.point.y, pos.z);
+            }
         }
     }
 }
