096e2d2540b8c6d6f351d52ea1607decc902e9ea
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-26 02:05:52 +0900

    水平方向の移動にも対応

--- Assets/Bright2/Scripts/ActorControllers/ActorMovement.cs
+++ Assets/Bright2/Scripts/ActorControllers/ActorMovement.cs
@@ -15,6 +15,9 @@ namespace HK.Bright2.ActorControllers
         private LayerMask raycastIncludeLayerMask = default;
 
         [SerializeField]
+        private Vector2 shrinkHorizontalRaycastBoxSize = default;
+
+        [SerializeField]
         private float gravity = default;
 
         private Actor actor;
@@ -36,7 +39,8 @@ namespace HK.Bright2.ActorControllers
             this.AddGravity();
 
             var t = this.actor.CachedTransform;
-            this.CheckGround();
+            this.CheckHorizontal();
+            this.CheckVertical();
             t.localPosition += new Vector3(this.velocity.x, this.velocity.y, 0.0f);
 
 #if UNITY_EDITOR
@@ -50,6 +54,10 @@ namespace HK.Bright2.ActorControllers
 
         private Vector2 lastVelocity;
 
+        private Vector2 lastHitHorizontalPoint;
+
+        private Vector2 lastHitVerticalPoint;
+
         void OnDrawGizmosSelected()
         {
             if(this.actor == null)
@@ -61,6 +69,12 @@ namespace HK.Bright2.ActorControllers
             var to = from + new Vector3(this.lastVelocity.x, this.lastVelocity.y, 0.0f);
             Gizmos.color = Color.red;
             Gizmos.DrawLine(from, to);
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(new Vector3(this.lastHitHorizontalPoint.x, this.lastHitHorizontalPoint.y, 0.0f), 0.1f);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(new Vector3(this.lastHitVerticalPoint.x, this.lastHitVerticalPoint.y, 0.0f), 0.1f);
         }
 #endif
 
@@ -69,30 +83,83 @@ namespace HK.Bright2.ActorControllers
             this.velocity += velocity;
         }
 
+        public void SetGravity(float gravity)
+        {
+            this.currentGravity = gravity;
+        }
+
         private void AddGravity()
         {
             this.currentGravity += this.gravity * Time.deltaTime;
             this.velocity += Vector2.down * this.currentGravity * Time.deltaTime;
         }
 
-        private void CheckGround()
+        private void CheckVertical()
         {
             var t = this.actor.CachedTransform;
             var pos = t.localPosition;
             var origin = new Vector2(pos.x, pos.y) + this.boxCollider2D.offset;
             var size = this.boxCollider2D.size;
+            size.x -= this.shrinkHorizontalRaycastBoxSize.x;
             var angle = t.localRotation.eulerAngles.z;
             var direction = Vector2.down;
             var distance = Mathf.Abs(this.velocity.y);
-            var raycastHit2D = Physics2D.BoxCast(origin, size, angle, direction, distance, raycastIncludeLayerMask.value);
-            if(raycastHit2D.transform != null)
+            var hit = Physics2D.BoxCast(origin, size, angle, direction, distance, raycastIncludeLayerMask.value);
+            if(hit.transform != null)
             {
-                var v = this.velocity;
-                v.y = 0.0f;
-                this.velocity = v;
-                this.currentGravity = 0.0f;
-                Debug.Log($"Hit {raycastHit2D.transform.name}");
-                t.localPosition = new Vector3(pos.x, raycastHit2D.point.y, pos.z);
+                var point = hit.point;
+                var diff = point.y - origin.y;
+
+                // 移動方向と接地点差分の方向が一致していたら接地処理をする
+                if((diff > 0 && this.velocity.y > 0) || (diff < 0 && this.velocity.y < 0))
+                {
+#if UNITY_EDITOR
+                    this.lastHitVerticalPoint = point;
+#endif
+                    var v = this.velocity;
+                    this.currentGravity = 0.0f;
+                    var newY = v.y < 0.0f ? point.y : point.y - size.y;
+                    t.localPosition = new Vector3(pos.x, newY, pos.z);
+                    v.y = 0.0f;
+                    this.velocity = v;
+                }
+            }
+        }
+
+        private void CheckHorizontal()
+        {
+            if(this.velocity.x == 0.0f)
+            {
+                return;
+            }
+
+            var t = this.actor.CachedTransform;
+            var pos = t.localPosition;
+            var origin = new Vector2(pos.x, pos.y) + this.boxCollider2D.offset;
+            var size = this.boxCollider2D.size;
+            size.y -= this.shrinkHorizontalRaycastBoxSize.y;
+            var angle = t.localRotation.eulerAngles.z;
+            var direction = this.velocity.x > 0.0f ? Vector2.right : Vector2.left;
+            var distance = Mathf.Abs(this.velocity.x);
+            var hit = Physics2D.BoxCast(origin, size, angle, direction, distance, raycastIncludeLayerMask.value);
+            if(hit.transform != null)
+            {
+                var point = hit.point;
+                var diff = point.x - pos.x;
+
+                if((diff > 0.0f && this.velocity.x > 0.0f) || (diff < 0.0f && this.velocity.x < 0.0f))
+                {
+#if UNITY_EDITOR
+                    this.lastHitHorizontalPoint = point;
+#endif
+
+                    var v = this.velocity;
+                    var offset = this.velocity.x > 0.0f ? -size.x * 0.5f : size.x * 0.5f;
+                    t.localPosition = new Vector3(point.x + offset, pos.y, pos.z);
+
+                    v.x = 0.0f;
+                    this.velocity = v;
+                }
             }
         }
     }
