0cbd3246ead9e449e592aa3a6911ab9af5a580b9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 00:05:27 +0900

    Actorに衝突したらダメージを受けるように

---
+++ Assets/Bright2/Scripts/ActorControllers/DamageActor.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using HK.Bright2.ActorControllers;
+using System.Collections.Generic;
+
+namespace HK.Bright2.ActorControllers
+{
+    /// <summary>
+    /// <see cref="Actor"/>にダメージを与えるギミックデコレーター
+    /// </summary>
+    public sealed class DamageActor : MonoBehaviour, IActorReactionOnTriggerEnter2D
+    {
+        [SerializeField]
+        private List<string> includeTags = default;
+        
+        [SerializeField]
+        private int damagePower = default;
+
+        [SerializeField]
+        private float knockbackPower = default;
+
+        private Actor owner;
+
+        void Awake()
+        {
+            this.owner = this.GetComponentInParent<Actor>();
+            Assert.IsNotNull(this.owner);
+        }
+
+        void IActorReactionOnTriggerEnter2D.Do(Actor actor)
+        {
+            // オーナーと衝突した場合は何もしない
+            if(this.owner == actor)
+            {
+                return;
+            }
+
+            if(!this.includeTags.Contains(actor.tag))
+            {
+                return;
+            }
+
+            actor.StatusController.TakeDamage(this.damagePower);
+            actor.Movement.SetGravity(this.GetKnockbackDirection(actor) * this.knockbackPower);
+        }
+
+        private Vector2 GetKnockbackDirection(Actor target)
+        {
+            var result = Vector2.up;
+            var diff = target.CachedTransform.position - this.owner.CachedTransform.position;
+            result.x = diff.x < 0.0f ? -1.0f : 1.0f;
+
+            return result.normalized;
+        }
+    }
+}
