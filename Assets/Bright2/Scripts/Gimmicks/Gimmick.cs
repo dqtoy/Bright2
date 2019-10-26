df3a22bc9a1ed59756f6d1f692ba1f5753aa5a20
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-26 18:39:51 +0900

    Gimmickクラス定義

---
+++ Assets/Bright2/Scripts/Gimmicks/Gimmick.cs
@@ -0,0 +1,25 @@
+﻿using HK.Framework;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GimmickControllers
+{
+    /// <summary>
+    /// <see cref="Actor"/>が生成するギミックの中核となるクラス
+    /// </summary>
+    public sealed class Gimmick : MonoBehaviour
+    {
+        private readonly static ObjectPoolBundle<Gimmick> pools = new ObjectPoolBundle<Gimmick>();
+
+        private ObjectPool<Gimmick> pool;
+
+        public Gimmick Rent()
+        {
+            var pool = pools.Get(this);
+            var clone = pool.Rent();
+            clone.pool = pool;
+
+            return clone;
+        }
+    }
+}
