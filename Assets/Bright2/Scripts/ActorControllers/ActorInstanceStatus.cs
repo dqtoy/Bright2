d99fddfe5953ac48886a3c79b842097d3c407bc0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-26 16:53:46 +0900

    動的なステータスを制御できるように

---
+++ Assets/Bright2/Scripts/ActorControllers/ActorInstanceStatus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers
+{
+    /// <summary>
+    /// <see cref="Actor"/>の動的なステータス
+    /// </summary>
+    public sealed class ActorInstanceStatus
+    {
+        /// <summary>
+        /// ジャンプした回数
+        /// </summary>
+        public int JumpCount { get; set; }
+
+        public ActorInstanceStatus(ActorContext context)
+        {
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/ActorControllers/ActorInstanceStatusController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers
+{
+    /// <summary>
+    /// <see cref="ActorInstanceStatus"/>を制御するクラス
+    /// </summary>
+    public sealed class ActorInstanceStatusController
+    {
+        private readonly ActorContext context;
+
+        private readonly ActorInstanceStatus status;
+
+        public ActorInstanceStatusController(ActorContext context)
+        {
+            this.context = context;
+            this.status = new ActorInstanceStatus(context);
+        }
+
+        public int JumpCount => this.status.JumpCount;
+
+        public void AddJumpCount()
+        {
+            this.status.JumpCount++;
+        }
+
+        public void ResetJumpCount()
+        {
+            this.status.JumpCount = 0;
+        }
+
+        public bool CanJump
+        {
+            get
+            {
+                return this.JumpCount < this.context.BasicStatus.LimitJumpCount;
+            }
+        }
+    }
+}
