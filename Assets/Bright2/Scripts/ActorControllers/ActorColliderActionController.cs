e4a9df7b964b9d4de7862450bf8828fbb2fa62ee
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-26 22:39:28 +0900

    Actorと衝突したら何かしらの処理を行えるように

---
+++ Assets/Bright2/Scripts/ActorControllers/ActorColliderActionController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers
+{
+    /// <summary>
+    /// <see cref="Actor"/>に衝突したオブジェクトに対してアクションを行うクラス
+    /// </summary>
+    public sealed class ActorColliderActionController : MonoBehaviour
+    {
+        private Actor owner;
+
+        void Awake()
+        {
+            this.owner = this.GetComponent<Actor>();
+            Assert.IsNotNull(this.owner);
+        }
+        
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            var reactionObjects = other.GetComponentsInChildren<IActorReactionOnTriggerEnter2D>();
+            foreach(var r in reactionObjects)
+            {
+                r.Do(this.owner);
+            }
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/ActorControllers/IActorReactionOnTriggerEnter2D.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers
+{
+    /// <summary>
+    /// OnTriggerEnter2D関数で<see cref="Actor"/>に対して様々な効果を付与するインターフェイス
+    /// </summary>
+    public interface IActorReactionOnTriggerEnter2D
+    {
+        /// <summary>
+        /// 処理を行う
+        /// </summary>
+        void Do(Actor actor);
+    }
+}
