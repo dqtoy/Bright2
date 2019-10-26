b1f3edee99dd1d6183421c3cc654cdd0d92082ab
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-26 23:10:56 +0900

    Gimmick経由でActorにダメージを与えられるように

---
+++ Assets/Bright2/Scripts/Gimmicks/Decorators/DamageActor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using HK.Bright2.ActorControllers;
+
+namespace HK.Bright2.GimmickControllers.Decorators
+{
+    /// <summary>
+    /// <see cref="Actor"/>にダメージを与えるギミックデコレーター
+    /// </summary>
+    public sealed class DamageActor : MonoBehaviour, IGimmickDecorator, IActorReactionOnTriggerEnter2D
+    {
+        [SerializeField]
+        private int damagePower = default;
+
+        private Actor gimmickOwner;
+
+        void IActorReactionOnTriggerEnter2D.Do(Actor actor)
+        {
+            // オーナーと衝突した場合は何もしない
+            if(this.gimmickOwner == actor)
+            {
+                return;
+            }
+
+            actor.StatusController.TakeDamage(this.damagePower);
+        }
+
+        void IGimmickDecorator.OnActivate(Gimmick owner, Actor gimmickOwner)
+        {
+            this.gimmickOwner = gimmickOwner;
+        }
+    }
+}
