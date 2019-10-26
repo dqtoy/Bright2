dfdf6983a641a1aeb2623ce1c3d81c2278d50518
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-26 11:13:12 +0900

    モデルを切り替えられるように

---
+++ Assets/Bright2/Scripts/ActorControllers/ActorModelNames.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers
+{
+    /// <summary>
+    /// <see cref="Actor"/>が持つモデルの名前を管理するクラス
+    /// </summary>
+    public static class ActorModelNames
+    {
+        public static readonly int Idle0 = Animator.StringToHash("Idle0");
+        public static readonly int Idle1 = Animator.StringToHash("Idle1");
+
+        public static readonly int Fall0 = Animator.StringToHash("Fall0");
+
+        public static readonly int Attack0 = Animator.StringToHash("Attack0");
+
+        public static readonly int Jump0 = Animator.StringToHash("Jump0");
+
+        public static readonly int Run0 = Animator.StringToHash("Run0");
+        public static readonly int Run1 = Animator.StringToHash("Run1");
+        public static readonly int Run2 = Animator.StringToHash("Run2");
+    }
+}
---
+++ Assets/Bright2/Scripts/ActorControllers/ActorModelSwitcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers
+{
+    /// <summary>
+    /// <see cref="Actor"/>が管理するモデルを切り替えるクラス
+    /// </summary>
+    public sealed class ActorModelSwitcher : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform modelParent;
+
+        private readonly Dictionary<int, Renderer> dictionary = new Dictionary<int, Renderer>();
+
+        private int currentModelId = 0;
+
+        void Awake()
+        {
+            for (var i = 0; i < this.modelParent.childCount; i++)
+            {
+                var child = this.modelParent.GetChild(i);
+                var model = child.GetComponentInChildren<Renderer>();
+                Assert.IsNotNull(model);
+                this.dictionary.Add(Animator.StringToHash(child.name), model);
+                model.enabled = false;
+            }
+
+            this.currentModelId = ActorModelNames.Idle0;
+            this.Change(this.currentModelId);
+        }
+
+        public void Change(int nextModelId)
+        {
+            this.dictionary[this.currentModelId].enabled = false;
+            this.currentModelId = nextModelId;
+            this.dictionary[this.currentModelId].enabled = true;
+        }
+    }
+}
