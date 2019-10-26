a7e6d937af27782ab2691e44bedf2aa768c37119
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 01:40:54 +0900

    プール可能なエフェクト追加

---
+++ Assets/Bright2/Scripts/GameSystems/PoolableEffect.cs
@@ -0,0 +1,38 @@
+﻿using HK.Framework;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2
+{
+    /// <summary>
+    /// プール可能なエフェクト
+    /// </summary>
+    public sealed class PoolableEffect : MonoBehaviour
+    {
+        private readonly static ObjectPoolBundle<PoolableEffect> pools = new ObjectPoolBundle<PoolableEffect>();
+
+        private ObjectPool<PoolableEffect> pool;
+
+        private float duration;
+
+        void Update()
+        {
+            this.duration -= Time.deltaTime;
+            if(this.duration <= 0.0f)
+            {
+                this.transform.SetParent(null);
+                this.pool.Return(this);
+            }
+        }
+
+        public PoolableEffect Rent(float delayReturnToPoolSeconds)
+        {
+            var pool = pools.Get(this);
+            var clone = pool.Rent();
+            clone.pool = pool;
+            clone.duration = delayReturnToPoolSeconds;
+
+            return clone;
+        }
+    }
+}
