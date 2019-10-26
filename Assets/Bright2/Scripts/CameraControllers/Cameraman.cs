02f58e25020c2b8f480f7b0650d100cc80b73f7e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-26 14:49:16 +0900

    とりあえずカメラ追従するだけ

---
+++ Assets/Bright2/Scripts/CameraControllers/Cameraman.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.CameraControllers
+{
+    /// <summary>
+    /// カメラの状態を制御するクラス
+    /// </summary>
+    public sealed class Cameraman : MonoBehaviour
+    {
+        [SerializeField]
+        private Camera controlledCamera = default;
+        public Camera Camera => this.controlledCamera;
+
+        public Transform CachedTransform { get; private set; }
+
+        void Awake()
+        {
+            this.CachedTransform = this.transform;
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/CameraControllers/GameCameraController.cs
@@ -0,0 +1,28 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.CameraControllers
+{
+    /// <summary>
+    /// ゲーム中に利用するカメラを制御するクラス
+    /// </summary>
+    public sealed class GameCameraController : MonoBehaviour
+    {
+        [SerializeField]
+        private Cameraman cameraman = default;
+
+        [SerializeField]
+        private Transform target = default;
+
+        void Awake()
+        {
+            this.LateUpdateAsObservable()
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    _this.cameraman.CachedTransform.position = _this.target.position;
+                });
+        }
+    }
+}
