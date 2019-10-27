f74ace507480f785c52e194c12e73b845faba214
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 01:58:59 +0900

    InvokeGameEventボタン作る

--- Assets/Bright2/Scripts/InputSystems/ActorControlInput.cs
+++ Assets/Bright2/Scripts/InputSystems/ActorControlInput.cs
@@ -48,8 +48,7 @@ namespace HK.Bright2.InputSystems
                 this.actor.Broker.Publish(RequestMove.Get(velocity));
             }
 
-            var vertical = Input.GetAxis("Vertical");
-            if(vertical > 0.0f)
+            if(Input.GetButtonDown("InvokeGameEvent"))
             {
                 this.actor.Broker.Publish(RequestInvokeGameEvent.Get());
             }
