e9e0cdba659f930aef62cf0dca9fbec8f1da3e59
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 11:47:36 +0900

    Start関数でActorを生成出来るように

---
+++ Assets/Bright2/Scripts/Extensions/Extensions.Actor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using HK.Bright2.ActorControllers;
+using HK.Framework.EventSystems;
+using HK.Bright2.GameSystems.Messages;
+
+namespace HK.Bright2.Extensions
+{
+    /// <summary>
+    /// <see cref="Actor"/>に関する拡張関数
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// <paramref name="prefab"/>を生成する
+        /// </summary>
+        public static Actor Spawn(this Actor prefab, Vector3 position)
+        {
+            var instance = Object.Instantiate(prefab);
+            instance.CachedTransform.position = position;
+
+            Broker.Global.Publish(SpawnedActor.Get(instance));
+
+            return instance;
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/GameSystems/ActorSpawnerOnStart.cs
@@ -0,0 +1,23 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.Extensions;
+using HK.Bright2.GameSystems.Messages;
+using HK.Framework.EventSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems
+{
+    /// <summary>
+    /// <see cref="ActorSpawnerOnStart.Start"/>関数のタイミングで<see cref="Actor"/>を生成するクラス
+    /// </summary>
+    public sealed class ActorSpawnerOnStart : MonoBehaviour
+    {
+        [SerializeField]
+        private Actor prefab = default;
+        
+        void Start()
+        {
+            prefab.Spawn(this.transform.position);
+        }
+    }
+}
