d679f55481c98de178182b9f6aaa657e2942674b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 10:48:03 +0900

    GameSystemをシングルトン化

--- Assets/Bright2/Scripts/GameSystems/GameSystem.cs
+++ Assets/Bright2/Scripts/GameSystems/GameSystem.cs
@@ -8,11 +8,23 @@ namespace HK.Bright2.GameSystems
     /// </summary>
     public sealed class GameSystem : MonoBehaviour
     {
+        private static GameSystem instance;
+        public static GameSystem Instance => instance;
+
         public PlayerManager PlayerManager { get; private set; }
 
         void Awake()
         {
+            Assert.IsNull(instance);
+            instance = this;
+
             this.PlayerManager = new PlayerManager();
+        }
+
+        void OnDestroy()
+        {
+            Assert.IsNotNull(instance);
+            instance = null;
         }
     }
 }
