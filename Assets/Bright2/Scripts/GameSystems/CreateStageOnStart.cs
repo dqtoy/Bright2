024587b049a9f3de8aa9b127804ef39215a8feac
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 00:55:57 +0900

    Stage.Base作っておく

---
+++ Assets/Bright2/Scripts/GameSystems/CreateStageOnStart.cs
@@ -0,0 +1,20 @@
+﻿using HK.Bright2.StageControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems
+{
+    /// <summary>
+    /// Start関数でステージを生成するクラス
+    /// </summary>
+    public sealed class CreateStageOnStart : MonoBehaviour
+    {
+        [SerializeField]
+        private Stage prefab = default;
+        
+        void Start()
+        {
+            Instantiate(this.prefab);
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/StageControllers/Stage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.StageControllers
+{
+    /// <summary>
+    /// ステージの中核となるクラス
+    /// </summary>
+    public sealed class Stage : MonoBehaviour
+    {
+    }
+}
