245ac7994bcb890e1bfcc8d096aaa637e89484a8
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 18:22:23 +0900

    ダメージを受けたことを通知するように

--- Assets/Bright2/Scripts/ActorControllers/ActorInstanceStatusController.cs
+++ Assets/Bright2/Scripts/ActorControllers/ActorInstanceStatusController.cs
@@ -85,6 +85,8 @@ namespace HK.Bright2.ActorControllers
 
             this.status.HitPoint.Value -= damage;
 
+            this.owner.Broker.Publish(TakedDamage.Get(damage, generationSource));
+
             // 死亡したら通知する
             if(this.status.HitPoint.Value <= 0)
             {
---
+++ Assets/Bright2/Scripts/ActorControllers/Messages/TakedDamage.cs
@@ -0,0 +1,22 @@
+﻿using HK.Framework.EventSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers.Messages
+{
+    /// <summary>
+    /// <see cref="Actor"/>がダメージを受けた際のメッセージ
+    /// </summary>
+    public sealed class TakedDamage : Message<TakedDamage, int, Vector2>
+    {
+        /// <summary>
+        /// 受けたダメージ量
+        /// </summary>
+        public int Damage => this.param1;
+
+        /// <summary>
+        /// ダメージが発生した地点
+        /// </summary>
+        public Vector2 GenerationSource => this.param2;
+    }
+}
