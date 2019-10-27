d059829ee8d3c141c711d1accfaf9a305af61c0c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 17:18:37 +0900

    プレイヤーのHPをUIで表示出来るように

---
+++ Assets/Bright2/Scripts/UI/HUDHitPointUIController.cs
@@ -0,0 +1,48 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.GameSystems.Messages;
+using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace HK.Bright2.UIControllers
+{
+    /// <summary>
+    /// HUDのヒットポイントUIを制御するクラス
+    /// </summary>
+    public sealed class HUDHitPointUIController : MonoBehaviour
+    {
+        [SerializeField]
+        private Slider slider = default;
+
+        void Awake()
+        {
+            Broker.Global.Receive<SpawnedActor>()
+                .Where(x => x.Actor.tag == Tags.Name.Player)
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.ObserveHitPoint(x.Actor.StatusController);
+                })
+                .AddTo(this);
+        }
+
+        private void ObserveHitPoint(ActorInstanceStatusController statusController)
+        {
+            Observable.Merge(
+                statusController.HitPoint,
+                statusController.HitPointMax
+            )
+            .SubscribeWithState2(this, statusController, (_, _this, s) =>
+            {
+                _this.UpdateSlider(s.HitPoint.Value, s.HitPointMax.Value);
+            })
+            .AddTo(this);
+        }
+
+        private void UpdateSlider(int hitPoint, int hitPointMax)
+        {
+            this.slider.value = (float)hitPoint / hitPointMax;
+        }
+    }
+}
