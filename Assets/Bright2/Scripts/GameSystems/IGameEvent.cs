3e28810d132dd6c0055fa23c0f36510043e30528
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 01:16:59 +0900

    IGameEvent実装

---
+++ Assets/Bright2/Scripts/GameSystems/IGameEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems
+{
+    /// <summary>
+    /// ゲームイベントのインターフェイス
+    /// </summary>
+    public interface IGameEvent
+    {
+        /// <summary>
+        /// ゲームイベントを実行する
+        /// </summary>
+        void Invoke();
+    }
+}
