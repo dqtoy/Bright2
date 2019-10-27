ab6f200e929f6ccbb9d3062afbcdade999c233d6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 11:06:03 +0900

    Tagsクラス定義

---
+++ Assets/Bright2/Scripts/Systems/Tags.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2
+{
+    /// <summary>
+    /// UnityのTagを管理するクラス
+    /// </summary>
+    public static class Tags
+    {
+        public class Name
+        {
+            public const string Player = "Player";
+            
+            public const string Enemy = "Enemy";
+        }
+    }
+}
