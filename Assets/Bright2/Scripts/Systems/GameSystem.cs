1026e9b063ea8934fc9d13bdb2eba30fb7c36ab7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 10:43:20 +0900

    プレイヤーを管理出来るように

---
+++ Assets/Bright2/Scripts/Systems/GameSystem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.Systems
+{
+    /// <summary>
+    /// ゲームの中核部分を担うクラス
+    /// </summary>
+    public sealed class GameSystem : MonoBehaviour
+    {
+        public PlayerManager PlayerManager { get; private set; }
+
+        void Awake()
+        {
+            this.PlayerManager = new PlayerManager();
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/Systems/PlayerManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2
+{
+    /// <summary>
+    /// プレイヤーを管理するクラス
+    /// </summary>
+    public sealed class PlayerManager
+    {
+    }
+}
