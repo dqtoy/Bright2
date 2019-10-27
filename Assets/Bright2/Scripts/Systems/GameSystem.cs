3bde7aa2d8885eee9891204c5ed245a0694d88b0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 10:44:09 +0900

    namespace更新

--- Assets/Bright2/Scripts/Systems/GameSystem.cs
+++ Assets/Bright2/Scripts/Systems/GameSystem.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace HK.Bright2.Systems
+namespace HK.Bright2.GameSystems
 {
     /// <summary>
     /// ゲームの中核部分を担うクラス
--- Assets/Bright2/Scripts/Systems/PlayerManager.cs
+++ Assets/Bright2/Scripts/Systems/PlayerManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.Assertions;
 
-namespace HK.Bright2
+namespace HK.Bright2.GameSystems
 {
     /// <summary>
     /// プレイヤーを管理するクラス
