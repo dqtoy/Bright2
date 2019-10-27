12ff50a99219b905f55960bc245143c93e701f5b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 12:46:24 +0900

    生成されたプレイヤーを管理するように

--- Assets/Bright2/Scripts/GameSystems/PlayerManager.cs
+++ Assets/Bright2/Scripts/GameSystems/PlayerManager.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using HK.Bright2.ActorControllers;
+using HK.Bright2.GameSystems.Messages;
+using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
 using UnityEngine.Assertions;
 
 namespace HK.Bright2.GameSystems
@@ -8,5 +13,27 @@ namespace HK.Bright2.GameSystems
     /// </summary>
     public sealed class PlayerManager
     {
+        private readonly List<Actor> players = new List<Actor>();
+        
+        private readonly CompositeDisposable streams = new CompositeDisposable();
+
+        public PlayerManager()
+        {
+            Broker.Global.Receive<SpawnedActor>()
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    var actor = x.Actor;
+                    if(actor.tag == Tags.Name.Player)
+                    {
+                        _this.players.Add(actor);
+                    }
+                })
+                .AddTo(this.streams);
+        }
+
+        ~PlayerManager()
+        {
+            this.streams.Dispose();
+        }
     }
 }
