ea5454ed8a961dfd584bf4001468b51bc4b922bb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-27 11:42:11 +0900

    Actorが生成されたことを通知出来るように

---
+++ Assets/Bright2/Scripts/GameSystems/Messages/SpawnedActor.cs
@@ -0,0 +1,15 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Framework.EventSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.Messages
+{
+    /// <summary>
+    /// <see cref="Actor"/>が生成されたことを通知するメッセージ
+    /// </summary>
+    public sealed class SpawnedActor : Message<SpawnedActor, Actor>
+    {
+        public Actor Actor => this.param1;
+    }
+}
