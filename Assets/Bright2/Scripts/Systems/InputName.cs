ee85ce6d850fdd004574e56b0a2eacf5173eeef0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 21:27:58 +0900

    InputName定義

---
+++ Assets/Bright2/Scripts/Systems/InputName.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2
+{
+    /// <summary>
+    /// 入力の名前
+    /// </summary>
+    public static class InputName
+    {
+        public const string Left = "Left";
+        public const string Right = "Right";
+        public const string Up = "Up";
+        public const string Down = "Down";
+
+        public const string Horizontal = "Horizontal";
+        public const string Vertical = "Vertical";
+
+        public const string Fire1 = "Fire1";
+        public const string Fire2 = "Fire2";
+        public const string Fire3 = "Fire3";
+
+        public const string Decide = "Decide";
+        public const string Cancel = "Cancel";
+    }
+}
