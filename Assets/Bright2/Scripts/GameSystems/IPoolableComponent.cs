06a356e9ee4ace3d28740d66705c0634995baef5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 16:53:53 +0900

    IPoolableComponent実装

---
+++ Assets/Bright2/Scripts/GameSystems/IPoolableComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2
+{
+    /// <summary>
+    /// オブジェクトプール可能なインターフェイス
+    /// </summary>
+    public interface IPoolableComponent
+    {
+        void Return();
+    }
+}
