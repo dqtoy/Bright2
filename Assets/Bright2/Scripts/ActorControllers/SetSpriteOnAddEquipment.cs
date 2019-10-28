78f87e7e7c5400983a6925ce3cd27e02d9db493a
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 12:55:23 +0900

    ドロップした装備品のアイコンを適用されるように

---
+++ Assets/Bright2/Scripts/ActorControllers/SetSpriteOnAddEquipment.cs
@@ -0,0 +1,20 @@
+﻿using HK.Bright2.Database;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers
+{
+    /// <summary>
+    /// <see cref="IAddEquipment.Setup(EquipmentRecord)"/>のタイミングでスプライトを設定するクラス
+    /// </summary>
+    public sealed class SetSpriteOnAddEquipment : MonoBehaviour, IAddEquipment
+    {
+        [SerializeField]
+        private SpriteRenderer controlledRenderer = default;
+
+        public void Setup(EquipmentRecord equipment)
+        {
+            this.controlledRenderer.sprite = equipment.Icon;
+        }
+    }
+}
