3d8a5c68c0a30848a7ddf3f6dbc800f5e3fc2423
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 16:54:57 +0900

    s/ReturnToPoolGimmick/ReturnToPool

---
+++ Assets/Bright2/Scripts/GameSystems/GiveDamageAdditionalEffects/ReturnToPool.cs
@@ -0,0 +1,21 @@
+﻿using HK.Bright2.GimmickControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.GiveDamageActorAdditionalEffects
+{
+    /// <summary>
+    /// 攻撃が当たった際にギミックをプールする
+    /// </summary>
+    [CreateAssetMenu(fileName = "ReturnToPool", menuName = "Bright2/GiveDamageActorAdditionalEffect/ReturnToPool")]
+    public sealed class ReturnToPool : GiveDamageActorAdditionalEffect
+    {
+        public override void Do(IGiveDamage giveDamage)
+        {
+            var gimmick = giveDamage.Root.GetComponent<IPoolableComponent>();
+            Assert.IsNotNull(gimmick, $"{giveDamage.Root}に{typeof(IPoolableComponent)}がありませんでした");
+
+            gimmick.Return();
+        }
+    }
+}
