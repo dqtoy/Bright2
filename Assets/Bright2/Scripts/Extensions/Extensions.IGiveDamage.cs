bc177f6e4b8ac124196f39552a68dc80027f8633
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 15:35:36 +0900

    IGiveDamage実装

---
+++ Assets/Bright2/Scripts/Extensions/Extensions.IGiveDamage.cs
@@ -0,0 +1,35 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.GameSystems;
+
+namespace HK.Bright2.Extensions
+{
+    /// <summary>
+    /// <see cref="IGiveDamage"/>に関する拡張関数
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static void TakeDamage(this IGiveDamage self, Actor target)
+        {
+            if(self.Owner == target)
+            {
+                return;
+            }
+
+            if(!self.IncludeTags.Contains(target.tag))
+            {
+                return;
+            }
+
+            if(target.StatusController.IsInfinity(self.GiveDamageObject))
+            {
+                return;
+            }
+
+            var generationSource = self.GiveDamageCollider.ClosestPoint(target.CachedTransform.position);
+
+            target.StatusController.TakeDamage(self.DamagePower, generationSource);
+            target.Movement.SetGravity(self.KnockbackDirection * self.KnockbackPower);
+            target.StatusController.AddInfinityStatus(self.GiveDamageObject, self.InfinitySeconds);
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/GameSystems/IGiveDamage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using HK.Bright2.ActorControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems
+{
+    /// <summary>
+    /// ダメージを与えるインターフェイス
+    /// </summary>
+    public interface IGiveDamage
+    {
+        /// <summary>
+        /// ダメージ
+        /// </summary>
+        int DamagePower { get; }
+
+        /// <summary>
+        /// ノックバック
+        /// </summary>
+        float KnockbackPower { get; }
+
+        /// <summary>
+        /// 攻撃が当たった際に相手に付与する無敵時間
+        /// </summary>
+        float InfinitySeconds { get; }
+
+        /// <summary>
+        /// ダメージを与える<see cref="Actor"/>
+        /// </summary>
+        Actor Owner { get; }
+
+        /// <summary>
+        /// 実際にダメージを与える<see cref="GameObject"/>
+        /// </summary>
+        GameObject GiveDamageObject { get; }
+
+        /// <summary>
+        /// 実際にダメージを与える<see cref="Collider2D"/>
+        /// </summary>
+        /// <value></value>
+        Collider2D GiveDamageCollider { get; }
+
+        /// <summary>
+        /// ダメージを与えられるタグリスト
+        /// </summary>
+        List<string> IncludeTags { get; }
+
+        /// <summary>
+        /// ノックバックの方向
+        /// </summary>
+        Vector2 KnockbackDirection { get; }
+    }
+}
