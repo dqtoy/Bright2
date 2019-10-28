4b1a8ee5f024768bc9caeced320d81178270b7e9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 16:40:02 +0900

    GiveDamageAdditionalEffect実装

---
+++ Assets/Bright2/Scripts/GameSystems/GiveDamageAdditionalEffects/GiveDamageActorAdditionalEffect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.GiveDamageActorAdditionalEffects
+{
+    /// <summary>
+    /// <see cref="Actor"/>にダメージを与えた際に追加で効果を与えるクラス
+    /// </summary>
+    public abstract class GiveDamageActorAdditionalEffect : ScriptableObject, IGiveDamageActorAdditionalEffect
+    {
+        public abstract void Do(IGiveDamage giveDamage);
+    }
+}
---
+++ Assets/Bright2/Scripts/GameSystems/GiveDamageAdditionalEffects/IGiveDamageActorAdditionalEffect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.GiveDamageActorAdditionalEffects
+{
+    /// <summary>
+    /// <see cref="Actor"/>にダメージを与えた際に追加で効果を与えるインターフェイス
+    /// </summary>
+    public interface IGiveDamageActorAdditionalEffect
+    {
+        void Do(IGiveDamage giveDamage);
+    }
+}
