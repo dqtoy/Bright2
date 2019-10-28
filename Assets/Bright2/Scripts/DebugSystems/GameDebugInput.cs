fd4063edf29a257032dc688a3ef3c06c36e5a56d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 20:11:21 +0900

    GameDebugInput作る

---
+++ Assets/Bright2/Scripts/DebugSystems/GameDebugInput.cs
@@ -0,0 +1,39 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.Database;
+using HK.Bright2.GameSystems.Messages;
+using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.DebugSystems
+{
+    /// <summary>
+    /// ゲームデバッグする入力を制御するクラス
+    /// </summary>
+    public sealed class GameDebugInput : MonoBehaviour
+    {
+        [SerializeField]
+        private WeaponRecord weapon = default;
+
+        private Actor player;
+
+        void Awake()
+        {
+            Broker.Global.Receive<SpawnedActor>()
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.player = x.Actor;
+                })
+                .AddTo(this);
+        }
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                this.player.StatusController.AddWeapon(this.weapon);
+            }
+        }
+    }
+}
