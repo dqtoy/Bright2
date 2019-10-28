1f6c78870602c34a720006fce6b6a71df183feae
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 22:55:43 +0900

    InstanceWeapon実装

---
+++ Assets/Bright2/Scripts/GameSystems/InstanceWeapon.cs
@@ -0,0 +1,19 @@
+﻿using HK.Bright2.Database;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems
+{
+    /// <summary>
+    /// 武器のインスタンスデータ
+    /// </summary>
+    public sealed class InstanceWeapon
+    {
+        public readonly WeaponRecord WeaponRecord;
+
+        public InstanceWeapon(WeaponRecord weaponRecord)
+        {
+            this.WeaponRecord = weaponRecord;
+        }
+    }
+}
