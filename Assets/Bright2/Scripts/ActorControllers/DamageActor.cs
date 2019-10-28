695f96139986ad317c19edcbd21f2b81e21130f5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 15:46:45 +0900

    DamageActorもIGiveDamageを継承するように

--- Assets/Bright2/Scripts/ActorControllers/DamageActor.cs
+++ Assets/Bright2/Scripts/ActorControllers/DamageActor.cs
@@ -2,26 +2,60 @@
 using UnityEngine.Assertions;
 using HK.Bright2.ActorControllers;
 using System.Collections.Generic;
+using HK.Bright2.GameSystems;
+using HK.Bright2.Extensions;
 
 namespace HK.Bright2.ActorControllers
 {
     /// <summary>
     /// <see cref="Actor"/>にダメージを与えるギミックデコレーター
     /// </summary>
-    public sealed class DamageActor : MonoBehaviour, IActorReactionOnTriggerEnter2D
+    public sealed class DamageActor : MonoBehaviour, IGiveDamage, IActorReactionOnTriggerEnter2D
     {
-        [SerializeField]
-        private List<string> includeTags = default;
-        
         [SerializeField]
         private int damagePower = default;
 
         [SerializeField]
         private float knockbackPower = default;
 
+        [SerializeField]
+        private float infinitySeconds = default;
+
+        [SerializeField]
+        private List<string> includeTags = default;
+
         private Collider2D controlledCollider;
 
         private Actor owner;
+
+        private Actor target;
+
+        int IGiveDamage.DamagePower => this.damagePower;
+
+        float IGiveDamage.KnockbackPower => this.knockbackPower;
+
+        float IGiveDamage.InfinitySeconds => this.infinitySeconds;
+
+        Actor IGiveDamage.Owner => this.owner;
+
+        GameObject IGiveDamage.GiveDamageObject => this.gameObject;
+
+        Collider2D IGiveDamage.GiveDamageCollider => this.controlledCollider;
+
+        List<string> IGiveDamage.IncludeTags => this.includeTags;
+
+        Vector2 IGiveDamage.KnockbackDirection
+        {
+            get
+            {
+                Assert.IsNotNull(this.target);
+                var result = Vector2.up;
+                var diff = this.target.CachedTransform.position - this.owner.CachedTransform.position;
+                result.x = diff.x < 0.0f ? -1.0f : 1.0f;
+
+                return result.normalized;
+            }
+        }
 
         void Awake()
         {
@@ -34,30 +68,8 @@ namespace HK.Bright2.ActorControllers
 
         void IActorReactionOnTriggerEnter2D.Do(Actor actor)
         {
-            // オーナーと衝突した場合は何もしない
-            if(this.owner == actor)
-            {
-                return;
-            }
-
-            if(!this.includeTags.Contains(actor.tag))
-            {
-                return;
-            }
-
-            var generationSource = this.controlledCollider.ClosestPoint(actor.CachedTransform.position);
-
-            actor.StatusController.TakeDamage(this.damagePower, generationSource);
-            actor.Movement.SetGravity(this.GetKnockbackDirection(actor) * this.knockbackPower);
-        }
-
-        private Vector2 GetKnockbackDirection(Actor target)
-        {
-            var result = Vector2.up;
-            var diff = target.CachedTransform.position - this.owner.CachedTransform.position;
-            result.x = diff.x < 0.0f ? -1.0f : 1.0f;
-
-            return result.normalized;
+            this.target = actor;
+            this.GiveDamage(actor);
         }
     }
 }
