43676b1bb5c272a8303dc6c3fbc6462a2edce3fb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-10-28 19:58:56 +0900

    とりあえずWeaponGridUI作ってみる

---
+++ Assets/Bright2/Scripts/UI/HUDWeaponGridScrollViewCellElement.cs
@@ -0,0 +1,31 @@
+﻿using HK.Bright2.Database;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace HK.Bright2.UIControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class HUDWeaponGridScrollViewCellElement : MonoBehaviour
+    {
+        [SerializeField]
+        private CanvasGroup canvasGroup = default;
+
+        [SerializeField]
+        private Image icon = default;
+
+        public void Setup(WeaponRecord weaponRecord)
+        {
+            this.icon.sprite = weaponRecord.Icon;
+            this.canvasGroup.alpha = 1.0f;
+        }
+
+        public void Deactive()
+        {
+            this.icon.sprite = null;
+            this.canvasGroup.alpha = 0.0f;
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/UI/HUDWeaponGridScrollViewItemData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using HK.Bright2.Database;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.UIControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class HUDWeaponGridScrollViewItemData
+    {
+        public List<WeaponRecord> Records { get; }
+
+        public HUDWeaponGridScrollViewItemData(List<WeaponRecord> records)
+        {
+            this.Records = records;
+        }
+    }
+}
