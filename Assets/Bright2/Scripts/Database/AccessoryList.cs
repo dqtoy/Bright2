4e55719ad71fa3fd9735357307e0726c4040315d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-02 00:03:26 +0900

    アクセサリー関連に必要なアセットを追加

---
+++ Assets/Bright2/Scripts/Database/AccessoryList.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.Database
+{
+    /// <summary>
+    /// アクセサリーマスターデータのリスト
+    /// </summary>
+    [CreateAssetMenu(menuName = "Bright2/MasterData/Accessory/List")]
+    public sealed class AccessoryList : MasterDataList<AccessoryRecord>
+    {
+    }
+}
---
+++ Assets/Bright2/Scripts/Database/AccessoryRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using HK.Bright2.GimmickControllers;
+using HK.Framework.Text;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.Database
+{
+    /// <summary>
+    /// アクセサリーマスターデータのレコード
+    /// </summary>
+    [CreateAssetMenu(menuName = "Bright2/MasterData/Accessory/Record")]
+    public sealed class AccessoryRecord : ScriptableObject, IMasterDataRecordId
+    {
+        public string Id => this.name;
+
+        [SerializeField]
+        private StringAsset.Finder accessoryName = default;
+        public string AccessoryName => this.accessoryName.Get;
+
+        [SerializeField]
+        private Sprite icon = default;
+        public Sprite Icon => this.icon;
+    }
+}
