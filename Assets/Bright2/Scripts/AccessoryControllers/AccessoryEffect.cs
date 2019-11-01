b19c318345c6ce1a44af11e93601d9ad95716904
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-02 00:09:11 +0900

    IAccessoryEffect関連追加

---
+++ Assets/Bright2/Scripts/AccessoryControllers/AccessoryEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.AccessoryControllers
+{
+    /// <summary>
+    /// アクセサリーの効果を持つ抽象クラス
+    /// </summary>
+    public sealed class AccessoryEffect : ScriptableObject, IAccessoryEffect
+    {
+    }
+}
---
+++ Assets/Bright2/Scripts/AccessoryControllers/IAccessoryEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.AccessoryControllers
+{
+    /// <summary>
+    /// アクセサリーの効果を持つインターフェイス
+    /// </summary>
+    public interface IAccessoryEffect
+    {
+    }
+}
