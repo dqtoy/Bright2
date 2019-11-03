e241e330d64ca4c623f49145eaf759b2a15dc523
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-03 17:41:38 +0900

    IWeightLottery追加

---
+++ Assets/Bright2/Scripts/Extensions/Extensions.IWeightLottery.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.Extensions
+{
+    /// <summary>
+    /// <see cref="IWeightLottery"/>に関する拡張関数
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 抽選を行う
+        /// </summary>
+        public static T Lottery<T>(this IEnumerable<T> list)
+            where T : IWeightLottery
+        {
+            var totalWeight = 0;
+            foreach(var i in list)
+            {
+                totalWeight += i.Weight;
+            }
+
+            var weight = Random.Range(0, totalWeight);
+            var minWeight = 0;
+            foreach(var i in list)
+            {
+                if(minWeight >= weight && weight < (minWeight + i.Weight))
+                {
+                    return i;
+                }
+
+                minWeight += i.Weight;
+            }
+
+            Assert.IsTrue(false, "抽選に失敗しました");
+
+            return default(T);
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/GameSystems/IWeightLottery.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2
+{
+    /// <summary>
+    /// 重み付けを行って抽選するインターフェイス
+    /// </summary>
+    public interface IWeightLottery
+    {
+        /// <summary>
+        /// 重み
+        /// </summary>
+        int Weight { get; }
+    }
+}
