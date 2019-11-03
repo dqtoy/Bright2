3bc9e0fbc3625af1e0d51bc9b80112f636b29aed
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-03 20:05:27 +0900

    NoneInput作っておく

---
+++ Assets/Bright2/Scripts/InputSystems/NoneInput.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.InputSystems
+{
+    /// <summary>
+    /// 入力されても何もしないクラス
+    /// </summary>
+    public sealed class NoneInput : IControllableUserInput
+    {
+        void IControllableUserInput.UpdateInput()
+        {
+        }
+    }
+}
