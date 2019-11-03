529b0d280f93c920c6216a7b444ff6987a12e329
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-04 03:08:34 +0900

    ステージ調整

--- Assets/Bright2/Scripts/StageControllers/SpawnActorOnActorReactionOnTriggerEnter.cs
+++ Assets/Bright2/Scripts/StageControllers/SpawnActorOnActorReactionOnTriggerEnter.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using HK.Bright2.ActorControllers;
+using HK.Bright2.ActorControllers.Messages;
 using HK.Bright2.Extensions;
+using UniRx;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -16,28 +18,43 @@ namespace HK.Bright2.StageControllers
         private List<string> includeTags = default;
 
         [SerializeField]
-        private Transform spawnPoint = default;
-
-        [SerializeField]
-        private List<Element> elements = default;
+        private List<ElementBundle> elementBundles = default;
 
         private bool isSpawned = false;
 
         void IActorReactionOnTriggerEnter2D.Do(Actor actor)
         {
-            if(this.isSpawned)
+            if (this.isSpawned)
             {
                 return;
             }
 
-            if(!this.includeTags.Contains(actor.tag))
+            if (!this.includeTags.Contains(actor.tag))
             {
                 return;
             }
 
-            var element = this.elements.Lottery();
-            element.Prefab.Spawn(this.spawnPoint.position);
             this.isSpawned = true;
+            this.SpawnRecursive(0);
+        }
+
+        private void SpawnRecursive(int index)
+        {
+            Assert.IsTrue(this.elementBundles.Count > index);
+
+            var elementBundle = this.elementBundles[index];
+            var element = elementBundle.Elements.Lottery();
+            var actor = element.Prefab.Spawn(element.SpawnPoint.position);
+            ++index;
+            if (this.elementBundles.Count > index)
+            {
+                actor.Broker.Receive<Died>()
+                    .SubscribeWithState2(this, index, (_, _this, i) =>
+                    {
+                        _this.SpawnRecursive(i);
+                    })
+                    .AddTo(actor);
+            }
         }
 
         [Serializable]
@@ -50,7 +67,19 @@ namespace HK.Bright2.StageControllers
             [SerializeField]
             private int weight = default;
 
+            [SerializeField]
+            private Transform spawnPoint = default;
+            public Transform SpawnPoint => this.spawnPoint;
+
             int IWeightLottery.Weight => this.weight;
+        }
+        
+        [Serializable]
+        public class ElementBundle
+        {
+            [SerializeField]
+            private List<Element> elements = default;
+            public List<Element> Elements => this.elements;
         }
     }
 }
