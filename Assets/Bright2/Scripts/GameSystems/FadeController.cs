af2c0d51c7753f79fbe3b83640c3bd43165c0f80
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-03 20:02:24 +0900

    フェードを制御出来るように

---
+++ Assets/Bright2/Scripts/GameSystems/FadeController.cs
@@ -0,0 +1,46 @@
+﻿using HK.Bright2.GameSystems.Messages.Fade;
+using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems
+{
+    /// <summary>
+    /// フェードを制御するクラス
+    /// </summary>
+    public sealed class FadeController : MonoBehaviour
+    {
+        [SerializeField]
+        private Fade fade = default;
+
+        [SerializeField]
+        private float fadeTimeSeconds = default;
+
+        void Awake()
+        {
+            Broker.Global.Receive<RequestFadeIn>()
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.fade.FadeIn(_this.fadeTimeSeconds, () =>
+                    {
+                        Broker.Global.Publish(EndFadeIn.Get());
+                    });
+                    Broker.Global.Publish(BeginFadeIn.Get());
+                })
+                .AddTo(this);
+            
+            Broker.Global.Receive<RequestFadeOut>()
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.fade.FadeOut(_this.fadeTimeSeconds, () =>
+                    {
+                        Broker.Global.Publish(EndFadeOut.Get());
+                    });
+                    Broker.Global.Publish(BeginFadeOut.Get());
+                })
+                .AddTo(this);
+        }
+
+    }
+}
---
+++ Assets/Bright2/Scripts/GameSystems/Messages/FadeMessages.cs
@@ -0,0 +1,48 @@
+﻿using HK.Framework.EventSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.Messages.Fade
+{
+    /// <summary>
+    /// フェードインをリクエストするメッセージ
+    /// </summary>
+    public sealed class RequestFadeIn : Message<RequestFadeIn>
+    {
+    }
+
+    /// <summary>
+    /// フェードアウトをリクエストするメッセージ
+    /// </summary>
+    public sealed class RequestFadeOut : Message<RequestFadeOut>
+    {
+    }
+
+    /// <summary>
+    /// フェードインを開始した際のメッセージ
+    /// </summary>
+    public sealed class BeginFadeIn : Message<BeginFadeIn>
+    {
+    }
+
+    /// <summary>
+    /// フェードインを終了した際のメッセージ
+    /// </summary>
+    public sealed class EndFadeIn : Message<EndFadeIn>
+    {
+    }
+
+    /// <summary>
+    /// フェードアウトを開始した際のメッセージ
+    /// </summary>
+    public sealed class BeginFadeOut : Message<BeginFadeOut>
+    {
+    }
+
+    /// <summary>
+    /// フェードアウトを終了した際のメッセージ
+    /// </summary>
+    public sealed class EndFadeOut : Message<EndFadeOut>
+    {
+    }
+}
