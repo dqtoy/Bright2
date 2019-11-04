c5800fc5f864812600ff4339f15620bef9a96f96
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-04 20:09:50 +0900

    通知UI実装

---
+++ Assets/Bright2/Scripts/UI/NotificationUIController.cs
@@ -0,0 +1,55 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.ActorControllers.Messages;
+using HK.Bright2.GameSystems.Messages;
+using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.UIControllers
+{
+    /// <summary>
+    /// 通知UIを制御するクラス
+    /// </summary>
+    public sealed class NotificationUIController : MonoBehaviour
+    {
+        [SerializeField]
+        private NotificationUIElement element = default;
+
+        [SerializeField]
+        private Transform viewport = default;
+
+        private Actor owner;
+
+        void Awake()
+        {
+            Broker.Global.Receive<SpawnedActor>()
+                .Where(x => x.Actor.CompareTag(Tags.Name.Player))
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.ObserveActor(x.Actor);
+                })
+                .AddTo(this);
+        }
+
+        private void ObserveActor(Actor actor)
+        {
+            this.owner = actor;
+            this.owner.Broker.Receive<AcquiredWeapon>()
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.CreateElement(x.Weapon.WeaponRecord.Icon, x.Weapon.WeaponRecord.WeaponName);
+                })
+                .AddTo(this)
+                .AddTo(this.owner);
+        }
+
+        private void CreateElement(Sprite sprite, string message)
+        {
+            var element = this.element.Rent();
+            element.Setup(sprite, message);
+            element.transform.SetParent(this.viewport, false);
+            element.transform.SetAsFirstSibling();
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/UI/NotificationUIElement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using HK.Framework;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace HK.Bright2.UIControllers
+{
+    /// <summary>
+    /// 通知UIの要素を制御するクラス
+    /// </summary>
+    public sealed class NotificationUIElement : MonoBehaviour, IPoolableComponent
+    {
+        [SerializeField]
+        private Image image = default;
+
+        [SerializeField]
+        private TextMeshProUGUI message = default;
+
+        [SerializeField]
+        private float visibleSeconds = default;
+
+        private ObjectPool<NotificationUIElement> pool;
+
+        private static readonly ObjectPoolBundle<NotificationUIElement> pools = new ObjectPoolBundle<NotificationUIElement>();
+
+        public void Setup(Sprite sprite, string message)
+        {
+            this.image.sprite = sprite;
+            this.message.text = message;
+            Observable.Timer(TimeSpan.FromSeconds(this.visibleSeconds))
+                .SubscribeWithState(this, (_, _this) =>
+                {
+                    _this.Return();
+                })
+                .AddTo(this);
+        }
+
+        public NotificationUIElement Rent()
+        {
+            var pool = pools.Get(this);
+            var clone = pool.Rent();
+            clone.pool = pool;
+
+            return clone;
+        }
+
+        public void Return()
+        {
+            this.pool.Return(this);
+        }
+    }
+}
