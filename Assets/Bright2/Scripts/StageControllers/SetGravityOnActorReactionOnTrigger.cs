3f1dd56d3435a265922f2cc1f135b88f3c517381
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-05 00:53:12 +0900

    トランポリンに乗ったら跳ねるように

---
+++ Assets/Bright2/Scripts/StageControllers/SetGravityOnActorReactionOnTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using HK.Bright2.ActorControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.StageControllers
+{
+    /// <summary>
+    /// <see cref="Actor"/>の重力を設定するクラス
+    /// </summary>
+    public sealed class SetGravityOnActorReactionOnTrigger : MonoBehaviour, IActorReactionOnTriggerEnter2D
+    {
+        [SerializeField]
+        private Vector2 gravity = default;
+
+        [SerializeField]
+        private List<string> includeTags = default;
+
+        void IActorReactionOnTriggerEnter2D.Do(Actor actor)
+        {
+            if(!this.includeTags.Contains(actor.tag))
+            {
+                return;
+            }
+
+            actor.Movement.SetGravity(this.gravity);
+        }
+    }
+}
