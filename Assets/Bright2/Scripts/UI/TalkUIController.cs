dd85934578169bb6dab7b85d043cae9e908e8ad1
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-07 22:42:09 +0900

    TalkUI作っておく

---
+++ Assets/Bright2/Scripts/UI/TalkUIController.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.UIControllers
+{
+    /// <summary>
+    /// トークUIを制御するクラス
+    /// </summary>
+    public sealed class TalkUIController : MonoBehaviour
+    {
+        [SerializeField]
+        private TextMeshProUGUI message = default;
+
+        private void SetMessage(string message)
+        {
+            this.message.text = message;
+        }
+    }
+}
