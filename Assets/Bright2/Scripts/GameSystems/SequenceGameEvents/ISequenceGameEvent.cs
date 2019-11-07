c8f35d419ed0e743d0358c8886f597eaca22dcca
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-07 22:26:47 +0900

    SequenceEvent関連定義

---
+++ Assets/Bright2/Scripts/GameSystems/SequenceGameEvents/ISequenceGameEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.SequenceGameEvents
+{
+    /// <summary>
+    /// 一連の流れを持つゲームイベントのインターフェイス
+    /// </summary>
+    public interface ISequenceGameEvent : IGameEvent
+    {
+        /// <summary>
+        /// イベントの要素リスト
+        /// </summary>
+        ISequenceGameEventElement[] Elements { get; }
+    }
+}
---
+++ Assets/Bright2/Scripts/GameSystems/SequenceGameEvents/ISequenceGameEventElement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.SequenceGameEvents
+{
+    /// <summary>
+    /// <see cref="ISequenveGameEvent"/>の要素のインターフェイス
+    /// </summary>
+    public interface ISequenceGameEventElement
+    {
+    }
+}
