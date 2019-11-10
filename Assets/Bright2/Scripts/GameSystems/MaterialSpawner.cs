a10797460f44592a6e27eca52bb47732edee95d9
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-10 17:16:31 +0900

    Actorが死亡したときに素材を落とすように

---
+++ Assets/Bright2/Scripts/GameSystems/MaterialSpawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using HK.Bright2.ActorControllers;
+using HK.Bright2.ActorControllers.Messages;
+using HK.Bright2.Database;
+using HK.Bright2.Extensions;
+using HK.Bright2.GameSystems.Messages;
+using HK.Bright2.GimmickControllers;
+using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems
+{
+    /// <summary>
+    /// 素材を生成するクラス
+    /// </summary>
+    public sealed class MaterialSpawner : MonoBehaviour, IDiedActorGimmickSpawner<DropMaterial, MaterialRecord>
+    {
+        [SerializeField]
+        private Gimmick gimmickPrefab = default;
+
+        [SerializeField]
+        private Vector2 diedSpawnOffset = default;
+
+        Gimmick IDiedActorGimmickSpawner<DropMaterial, MaterialRecord>.GimmickPrefab => this.gimmickPrefab;
+
+        void Awake()
+        {
+            this.ReceiveSpawnedActor(this.gameObject);
+
+            Broker.Global.Receive<RequestSpawnMaterial>()
+                .SubscribeWithState(this, (x, _this) =>
+                {
+                    _this.Setup(_this.CreateGimmick(x.Owner, x.SpawnPosition), x.MaterialRecord);
+                })
+                .AddTo(this);
+        }
+
+        Vector3 IDiedActorGimmickSpawner<DropMaterial, MaterialRecord>.GetSpawnPoint(Actor actor)
+        {
+            return actor.CachedTransform.position + new Vector3(this.diedSpawnOffset.x, this.diedSpawnOffset.y, 0.0f);
+        }
+
+        public IEnumerable<DropMaterial> GetDropData(Actor actor)
+        {
+            return actor.Context.DropItems.Materials;
+        }
+
+        public void Setup(Gimmick gimmick, MaterialRecord dropData)
+        {
+            foreach (var i in gimmick.GetComponentsInChildren<IAddMaterial>())
+            {
+                i.Setup(dropData);
+            }
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/GameSystems/Messages/RequestSpawnMaterial.cs
@@ -0,0 +1,20 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.Database;
+using HK.Framework.EventSystems;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.Messages
+{
+    /// <summary>
+    /// 素材の生成をリクエストするメッセージ
+    /// </summary>
+    public sealed class RequestSpawnMaterial : Message<RequestSpawnMaterial, Actor, MaterialRecord, Vector3>
+    {
+        public Actor Owner => this.param1;
+
+        public MaterialRecord MaterialRecord => this.param2;
+
+        public Vector3 SpawnPosition => this.param3;
+    }
+}
