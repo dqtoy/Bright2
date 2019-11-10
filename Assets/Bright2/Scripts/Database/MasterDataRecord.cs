fcba9a057ded109f203e6850de65bfd0bfbb327b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-10 21:25:10 +0900

    MasterDataRecordクラス作っておく

---
+++ Assets/Bright2/Scripts/Database/MasterDataRecord.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.Database
+{
+    /// <summary>
+    /// マスターデータのレコードの抽象クラス
+    /// </summary>
+    public abstract class MasterDataRecord : ScriptableObject, IMasterDataRecord
+    {
+    }
+}
