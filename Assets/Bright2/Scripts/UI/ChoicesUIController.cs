17f7b041a3d035f596a182a595bd5bd32b6e56c6
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-10 13:48:54 +0900

    とりあえず選択肢UIを制御出来るように

---
+++ Assets/Bright2/Scripts/UI/ChoicesUIController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.UIControllers
+{
+    /// <summary>
+    /// 選択肢UIを制御するクラス
+    /// </summary>
+    public sealed class ChoicesUIController : MonoBehaviour
+    {
+        [SerializeField]
+        private ChoicesUIElement elementPrefab = default;
+
+        
+    }
+}
---
+++ Assets/Bright2/Scripts/UI/ChoicesUIElement.cs
@@ -0,0 +1,39 @@
+﻿using HK.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.UIControllers
+{
+    /// <summary>
+    /// 選択肢UIの要素を制御するクラス
+    /// </summary>
+    public sealed class ChoicesUIElement : MonoBehaviour, IPoolableComponent
+    {
+        [SerializeField]
+        private TextMeshProUGUI message = default;
+
+        private static readonly ObjectPoolBundle<ChoicesUIElement> pools = new ObjectPoolBundle<ChoicesUIElement>();
+
+        private ObjectPool<ChoicesUIElement> pool;
+
+        public ChoicesUIElement Rent()
+        {
+            var pool = pools.Get(this);
+            var clone = pool.Rent();
+            clone.pool = pool;
+
+            return clone;
+        }
+
+        public void Return()
+        {
+            this.pool.Return(this);
+        }
+
+        public void Setup(string message)
+        {
+            this.message.text = message;
+        }
+    }
+}
