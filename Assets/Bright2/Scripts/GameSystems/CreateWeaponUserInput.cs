ce267b53725a5685ac84fdbc0b715372591aa208
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-15 23:58:47 +0900

    CreateWeaponUserInput関連追加

---
+++ Assets/Bright2/Scripts/GameSystems/CreateWeaponUserInput.cs
@@ -0,0 +1,24 @@
+﻿using HK.Bright2.GameSystems.Messages;
+using HK.Framework.EventSystems;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems
+{
+    /// <summary>
+    /// ユーザーの入力によって<see cref="Actor"/>に対して武器を追加するクラス
+    /// </summary>
+    public sealed class CreateWeaponUserInput : MonoBehaviour
+    {
+        void Awake()
+        {
+            Broker.Global.Receive<RequestCreateWeaponUserInput>()
+                .SubscribeWithState(this, (x, _this) =>
+                {
+
+                })
+                .AddTo(this);
+        }
+    }
+}
