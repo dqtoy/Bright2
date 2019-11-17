520ac46dd0bf900795fcbf786eb912be03b4c4a0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-18 00:02:10 +0900

    IMasterDataRecordRecipe追加

---
+++ Assets/Bright2/Scripts/Database/IMasterDataRecordRecipe.cs
@@ -0,0 +1,17 @@
+﻿using HK.Bright2.ItemControllers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.Database
+{
+    /// <summary>
+    /// 何かしらのアイテムを生成するレシピを持つマスターデータ
+    /// </summary>
+    public interface IMasterDataRecordRecipe
+    {
+        /// <summary>
+        /// 必要なアイテム
+        /// </summary>
+        NeedItems NeedItems { get; }
+    }
+}
