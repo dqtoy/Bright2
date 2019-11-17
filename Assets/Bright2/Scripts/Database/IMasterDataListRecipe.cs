a33cad76a704cc3d1b442be155e640e73b689ae0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-18 00:09:26 +0900

    IMasterDataListRecipe追加

---
+++ Assets/Bright2/Scripts/Database/IMasterDataListRecipe.cs
@@ -0,0 +1,9 @@
+﻿namespace HK.Bright2.Database
+{
+    /// <summary>
+    /// <see cref="IMasterDataRecordRecipe"/>を管理する<see cref="MasterDataList{E}"/>
+    /// </summary>
+    public interface IMasterDataListRecipe<E> : IMasterDataList<E> where E : IMasterDataRecord, IMasterDataRecordRecipe
+    {
+    }
+}
