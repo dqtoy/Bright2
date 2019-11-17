843defae310c79162375c94503ca9402952e893e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-18 00:17:55 +0900

    Extensions.IMasterDataListRecipe追加

---
+++ Assets/Bright2/Scripts/Extensions/Extensions.IMasterDataListRecipe.cs
@@ -0,0 +1,33 @@
+﻿using HK.Bright2.Database;
+using System.Collections.Generic;
+using HK.Bright2.ItemControllers;
+
+namespace HK.Bright2.Extensions
+{
+    /// <summary>
+    /// <see cref="IMasterDataListRecipe{E}"/>に関する拡張関数
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 表示可能なレシピのリストを返す
+        /// </summary>
+        public static IReadOnlyList<E> GetViewableRecipes<E>(this IMasterDataListRecipe<E> self, Inventory inventory)
+            where E : IMasterDataRecordRecipe
+        {
+            var result = new List<E>();
+
+            foreach (var r in self.Records)
+            {
+                if (!r.NeedItems.IsViewableList(inventory))
+                {
+                    continue;
+                }
+
+                result.Add(r);
+            }
+
+            return result;
+        }
+    }
+}
