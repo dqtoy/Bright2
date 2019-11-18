7067441cc116b8fc8a7f1a3c1abf6a2bed514329
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-19 00:41:26 +0900

    武器強化のシーケンスイベントを実装

---
+++ Assets/Bright2/Scripts/GameSystems/SequenceGameEvents/AttachItemModifierToWeaponUserInput.cs
@@ -0,0 +1,33 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.GameSystems.Messages;
+using HK.Framework.EventSystems;
+using HK.Framework.Text;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.SequenceGameEvents
+{
+    /// <summary>
+    /// 武器にアイテム修飾を装着するシーケンスゲームイベント
+    /// </summary>
+    [CreateAssetMenu(menuName = "Bright2/SequenceGameEvent/Element/AttachItemModifierToWeaponUserInput")]
+    public sealed class AttachItemModifierToWeaponUserInput : SequenceGameEventElement
+    {
+        [SerializeField]
+        private SequenceGameEventElement nextElement = default;
+
+        public override void Invoke(ISequenceGameEvent owner, Actor invoker)
+        {
+            Broker.Global.Publish(AttachItemModifierToWeaponUserInputMessages.Request.Get(invoker));
+
+            Broker.Global.Receive<AttachItemModifierToWeaponUserInputMessages.End>()
+                .Take(1)
+                .SubscribeWithState3(this, owner, invoker, (_, _this, _owner, _invoker) =>
+                {
+                    _owner.Next(_this.nextElement, _invoker);
+                })
+                .AddTo(invoker);
+        }
+    }
+}
