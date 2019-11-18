cc57ff29dc340e5db0fca4026fe9a9a3172866ea
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-19 00:33:31 +0900

    武器作成のシーケンスイベントを実装

---
+++ Assets/Bright2/Scripts/GameSystems/SequenceGameEvents/CreateWeaponUserInput.cs
@@ -0,0 +1,33 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.GameSystems.Messages;
+using HK.Framework.EventSystems;
+using HK.Framework.Text;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.GameSystems.SequenceGameEvents
+{
+    /// <summary>
+    /// 武器作成を行うシーケンスゲームイベント
+    /// </summary>
+    [CreateAssetMenu(menuName = "Bright2/SequenceGameEvent/Element/CreateWeaponUserInput")]
+    public sealed class CreateWeaponUserInput : SequenceGameEventElement
+    {
+        [SerializeField]
+        private SequenceGameEventElement nextElement = default;
+
+        public override void Invoke(ISequenceGameEvent owner, Actor invoker)
+        {
+            Broker.Global.Publish(CreateWeaponUserInputMessages.Request.Get(invoker));
+
+            Broker.Global.Receive<CreateWeaponUserInputMessages.End>()
+                .Take(1)
+                .SubscribeWithState3(this, owner, invoker, (_, _this, _owner, _invoker) =>
+                {
+                    _owner.Next(_this.nextElement, _invoker);
+                })
+                .AddTo(invoker);
+        }
+    }
+}
