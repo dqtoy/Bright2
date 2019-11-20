3fa8098659a420b932d26e0b9203d58f55080e2b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-21 00:30:03 +0900

    ISyncDirectionで方向の同期をとれるように

---
+++ Assets/Bright2/Scripts/GameSystems/ISyncDirection.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2
+{
+    /// <summary>
+    /// <see cref="Constants.Direction"/>から同期を取るインターフェイス
+    /// </summary>
+    public interface ISyncDirection
+    {
+        /// <summary>
+        /// 同期を取る
+        /// </summary>
+        void Sync(Constants.Direction direction);
+    }
+}
---
+++ Assets/Bright2/Scripts/GameSystems/SyncDirectionLocalScale.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2
+{
+    /// <summary>
+    /// <see cref="Constants.Direction"/>から<see cref="Transform.localScale"/>を設定するクラス
+    /// </summary>
+    public sealed class SyncDirectionLocalScale : MonoBehaviour, ISyncDirection
+    {
+        [SerializeField]
+        private Vector3 leftScale = default;
+
+        [SerializeField]
+        private Vector3 rightScale = default;
+
+        void ISyncDirection.Sync(Constants.Direction direction)
+        {
+            switch(direction)
+            {
+                case Constants.Direction.Left:
+                    this.transform.localScale = this.leftScale;
+                    break;
+                case Constants.Direction.Right:
+                    this.transform.localScale = this.rightScale;
+                    break;
+                default:
+                    Assert.IsTrue(false, $"{direction}は未対応です");
+                    break;
+            }
+        }
+    }
+}
