58ce6ff457c07d5f29db6cbe2507598000aea838
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-11-26 23:10:51 +0900

    大事なアイテムマスターデータ追加

---
+++ Assets/Bright2/Scripts/Database/ImportantItemList.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.Database
+{
+    /// <summary>
+    /// 大事なアイテムマスターデータのリスト
+    /// </summary>
+    [CreateAssetMenu(menuName = "Bright2/MasterData/ImportantItem/List")]
+    public sealed class ImportantItemList : MasterDataList<ImportantItemRecord>
+    {
+    }
+}
---
+++ Assets/Bright2/Scripts/Database/ImportantItemRecord.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using HK.Bright2.GameSystems;
+using HK.Bright2.ItemControllers;
+using HK.Bright2.UIControllers;
+using HK.Framework.Text;
+using UnityEngine;
+
+namespace HK.Bright2.Database
+{
+    /// <summary>
+    /// 大事なアイテムマスターデータのレコード
+    /// </summary>
+    [CreateAssetMenu(menuName = "Bright2/MasterData/ImportantItem/Record")]
+    public sealed class ImportantItemRecord : MasterDataRecord, IMasterDataRecordId, IIconHolder, INameHolder, IViewableList
+    {
+        public string Id => this.name;
+
+        [SerializeField]
+        private StringAsset.Finder accessoryName = default;
+        public string AccessoryName => this.accessoryName.Get;
+
+        [SerializeField]
+        private Sprite icon = default;
+        public Sprite Icon => this.icon;
+
+        [SerializeField]
+        private List<ItemModifier> modifiers = default;
+        public List<ItemModifier> Modifiers => this.modifiers;
+
+        string INameHolder.Name => this.AccessoryName;
+    }
+}
