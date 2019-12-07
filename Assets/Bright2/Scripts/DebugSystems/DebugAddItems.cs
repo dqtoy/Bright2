d5215f70d65209bfacfea8f260766b25c3f2e3fc
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-12-08 01:41:41 +0900

    面倒だからデバッグですべてのアイテムを所持できるように

--- Assets/Bright2/Scripts/DebugSystems/DebugAddItems.cs
+++ Assets/Bright2/Scripts/DebugSystems/DebugAddItems.cs
@@ -14,37 +14,24 @@ namespace HK.Bright2.DebugSystems
     /// </summary>
     public sealed class DebugAddItems : MonoBehaviour
     {
-        [SerializeField]
-        private List<WeaponRecord> possessionWeapons = default;
-
-        [SerializeField]
-        private List<AccessoryRecord> possessionAccessories = default;
-
-        [SerializeField]
-        private List<int> equippedAccessories = default;
-
         void Awake()
         {
             Broker.Global.Receive<SpawnedActor>()
                 .Where(x => x.Actor.CompareTag(Tags.Name.Player))
                 .SubscribeWithState(this, (x, _this) =>
                 {
-                    foreach(var w in _this.possessionWeapons)
+                    var masterData = GameSystem.Instance.MasterData;
+                    foreach(var w in masterData.Weapon.Records)
                     {
                         x.Actor.StatusController.AddWeapon(w);
                     }
 
-                    foreach(var a in _this.possessionAccessories)
+                    foreach(var a in masterData.Accessory.Records)
                     {
                         x.Actor.StatusController.AddAccessory(a);
                     }
 
-                    for (var i = 0; i < _this.equippedAccessories.Count; i++)
-                    {
-                        x.Actor.StatusController.ChangeEquippedAccessory(i, _this.equippedAccessories[i]);
-                    }
-
-                    foreach(var m in GameSystem.Instance.MasterData.Material.Records)
+                    foreach(var m in masterData.Material.Records)
                     {
                         x.Actor.StatusController.AddMaterial(m, 2);
                     }
