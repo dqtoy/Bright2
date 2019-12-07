493deda03da03590b5462d9875f4a1e3db5dcb5c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-12-07 16:33:38 +0900

    JumpするAI実装

---
+++ Assets/Bright2/Scripts/ActorControllers/AI/Elements/Jump.cs
@@ -0,0 +1,27 @@
+﻿using HK.Bright2.ActorControllers.Messages;
+using HK.Bright2.Extensions;
+using HK.Bright2.GameSystems;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.Bright2.ActorControllers.AIControllers
+{
+    /// <summary>
+    /// ジャンプするAI
+    /// </summary>
+    [CreateAssetMenu(fileName = "Jump", menuName = "Bright2/AI/Elements/Jump")]
+    public sealed class Jump : ScriptableAIElement
+    {
+        public override void Enter(Actor owner)
+        {
+            owner.UpdateAsObservable()
+                .SubscribeWithState2(this, owner, (_, _this, _owner) =>
+                {
+                    _owner.Broker.Publish(RequestJump.Get());
+                })
+                .AddTo(this.events);
+        }
+    }
+}
