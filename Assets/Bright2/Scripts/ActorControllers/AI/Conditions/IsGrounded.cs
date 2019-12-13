713bde49634df5f9bd6ea20006c62af16cae6595
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-12-14 02:53:36 +0900

    0106実装中

---
+++ Assets/Bright2/Scripts/ActorControllers/AI/Conditions/IsGrounded.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace HK.Bright2.ActorControllers.AIControllers
+{
+    /// <summary>
+    /// 接地している場合に条件を満たす<see cref="ScriptableAICondition"/>
+    /// </summary>
+    [CreateAssetMenu(fileName = "Condition.IsGrounded.asset", menuName = "Bright2/AI/Conditions/IsGrounded")]
+    public sealed class IsGrounded : ScriptableAICondition
+    {
+        /// <summary>
+        /// 接地していない時に条件を満たすか
+        /// </summary>
+        [SerializeField]
+        private bool isReverse = false;
+
+        public override IObservable<Unit> Satisfy(Actor owner, ActorAIController ownerAI)
+        {
+            return owner.UpdateAsObservable().Where(_ => this.SatisfyFromDistance(owner));
+        }
+
+        private bool SatisfyFromDistance(Actor owner)
+        {
+            return owner.Movement.IsGrounded == !this.isReverse;
+        }
+    }
+}
