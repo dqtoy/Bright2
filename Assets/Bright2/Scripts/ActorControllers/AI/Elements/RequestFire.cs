28b5ed9621c62a7d73b3a62914862f7a4137b10c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2019-12-17 00:28:48 +0900

    0108完成

---
+++ Assets/Bright2/Scripts/ActorControllers/AI/Elements/RequestFire.cs
@@ -0,0 +1,28 @@
+﻿using HK.Bright2.ActorControllers.Messages;
+using UniRx;
+using UnityEngine;
+
+namespace HK.Bright2.ActorControllers.AIControllers
+{
+    /// <summary>
+    /// 攻撃のリクエストを行うAI
+    /// </summary>
+    [CreateAssetMenu(fileName = "RequestFire", menuName = "Bright2/AI/Elements/RequestFire")]
+    public sealed class RequestFire : ScriptableAIElement
+    {
+        [SerializeField]
+        private int equipmentIndex = default;
+
+        public override void Enter(Actor owner, ActorAIController ownerAI)
+        {
+            base.Enter(owner, ownerAI);
+            
+            this.GetObserver(owner)
+                .SubscribeWithState2(this, owner, (_, _this, _owner) =>
+                {
+                    _owner.Broker.Publish(Messages.RequestFire.Get(_this.equipmentIndex));
+                })
+                .AddTo(this.events);
+        }
+    }
+}
---
+++ Assets/Bright2/Scripts/GimmickControllers/Decorators/LookAtRandomPlayer.cs
@@ -0,0 +1,18 @@
+﻿using HK.Bright2.ActorControllers;
+using HK.Bright2.GameSystems;
+using UnityEngine;
+
+namespace HK.Bright2.GimmickControllers.Decorators
+{
+    /// <summary>
+    /// ランダムに選択されたプレイヤーの方向へ注視する<see cref="IGimmickDecorator"/>
+    /// </summary>
+    public sealed class LookAtRandomPlayer : MonoBehaviour, IGimmickDecorator
+    {
+        void IGimmickDecorator.OnActivate(Gimmick owner, Actor gimmickOwner)
+        {
+            var target = GameSystem.Instance.ActorManager.GetRandomPlayer();
+            this.transform.right = target.CachedTransform.position - this.transform.position;
+        }
+    }
+}
